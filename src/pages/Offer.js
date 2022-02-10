80184ed7a91ef413efe9636aa3855285a777ea7c
Marielle723 <https://github.com/Marielle723>
2022-02-10 02:08:17 +0100

    stuck at trying to recuperate the id

--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -1,5 +1,39 @@
+import { useParams } from "react-router-dom";
+
+import Header from "../components/Header";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Offer = () => {
-  return <div>Product Component</div>;
+  const { id } = useParams();
+  console.log(id);
+
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers/`"
+        );
+        console.log(response.data.id);
+        setData(response.data.id);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response); // contrairement au error.message d'express
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Offer;
