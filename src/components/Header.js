344b49166c78f2cfda621b83878d81a5d92f24bd
Marielle723 <https://github.com/Marielle723>
2022-02-10 01:18:37 +0100

    second step home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,21 @@ import VintedLogo from "../assets/images/Vinted-logo.svg.png";
 
 const Header = () => {
   return (
-    <header>
-      <img src={VintedLogo} alt="Logo de vinted" />
-      <input placeholder="Recherche des articles" name="search_input" />
-      <button>S'inscrire</button>
-      <button>Se connecter</button>
-      <button>Vends tes articles</button>
-    </header>
+    <>
+      <header>
+        <img src={VintedLogo} alt="Logo de vinted" />
+        <input placeholder="Recherche des articles" name="search_input" />
+        <button>S'inscrire</button>
+        <button>Se connecter</button>
+        <button>Vends tes articles</button>
+      </header>
+      <div className="big-picture">
+        <div className="box-in-big-picture">
+          <h2>Prêt à faire du tri dans vos placards?</h2>
+          <button>Commencez à vendre</button>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,69 @@
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  return (
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response); // contrairement au error.message d'express
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
     <div>
       <Header />
-      <Link to={"/offer"}>Go to offer</Link>
+
+      {data.offers.map((offer, index) => {
+        return (
+          <div className="home-wrapper">
+            <div key={offer._id} className="home-offers">
+              <div className="owner">
+                <img src={`${offer.owner.account.avatar.secure_url}`} />
+                {offer.owner.account.username}
+              </div>
+
+              <img
+                className="product-image"
+                src={`${offer.product_image.secure_url}`}
+                alt="photo du produit"
+              />
+            </div>
+          </div>
+        );
+      })}
+
+      {/* <Link to={"/offer"}>Go to offer</Link> */}
+      {/* {
+        <ul className="home-offers">
+          {data.offers.map((offer, index) => {
+            return (
+              <li key={index}>
+                <div className="owner">
+                  {offer.owner.account.avatar}
+                  {offer.owner.account.username}
+                </div>
+                {offer.product_pictures}
+              </li>
+            );
+          })}
+        </ul> */}
+      {/* } */}
     </div>
   );
 };
